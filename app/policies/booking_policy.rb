7dcd14085bc2a7c5a4b077dccc94b89739a64a13
Kirsty Leishman <leishman.kirsty@gmail.com>
2018-02-27 18:51:38 +0000

    added bookings

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -4,4 +4,12 @@ class BookingPolicy < ApplicationPolicy
       scope
     end
   end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :lessons
+  resources :lessons do
+    resources :bookings, only: [:new, :create]
+  end
 end
