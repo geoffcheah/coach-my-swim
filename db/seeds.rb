00c6759d921de4c573e9d896705d9d5c2ca8c9e4
Geoffrey Cheah <geoffcheah@gmail.com>
2018-02-28 17:07:00 +0000

    created seed file. MUST REMEMBER TO DELETE LESSON WITH NO USER

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,19 +13,30 @@ User.destroy_all
 
 puts "Seeding Users and Lessons"
 
+user = User.create(email: "geoffcheah@gmail.com", password: "123456", first_name: "Geoffrey", last_name: "Cheah", photo: "File_001" )
+lesson = Lesson.create(user: user,
+    price: 10,
+    bio: "Almost drowned when I was little",
+    speciality: "Freestyle",
+    ability: "Level 3")
+
+User.create(email: "a.p.hughes@live.com", password: "123456", first_name: "Alex", last_name: "Hughes", photo: "alex" )
+
+u = User.create(email: "leishman.kirsty", password: "123456", first_name: "Kirsty", last_name: "Leishman", photo: "http://res.cloudinary.com/dl8jzr6kn/image/upload/v1519835396/profile_default.png")
+l = Lesson.create(user: u,
+    price: 10,
+    bio: "I won every medal possible",
+    speciality: "Backstroke",
+    ability: "Level 2")
+
 5.times do
-  user = User.create(email: Faker::Internet.email, password: "123456", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name )
-  lesson = Lesson.create(user: user,
+  us = User.create(email: Faker::Internet.email, password: "123456", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, photo: "http://res.cloudinary.com/dl8jzr6kn/image/upload/v1519835396/profile_default.png")
+  le = Lesson.create(user: us,
     price: 10,
     bio: "Almost drowned when I was little",
     speciality: "Freestyle",
     ability: "Level 1")
 end
 
-puts "creating swimmers"
-
-5.times do
-  User.create(email: Faker::Internet.email, password: "123456", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-end
 
 puts "finished"
