32c26a895c6c69235120076fce42eed406708989
Geoffrey Cheah <geoffcheah@gmail.com>
2018-03-01 12:18:44 +0000

    added inclusion for rating: only between 1-5

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :lesson
   validates :content, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: [1, 2, 3, 4, 5] }
 end
