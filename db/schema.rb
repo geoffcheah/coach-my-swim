14b1f7ce6630e0a1b3c2ecda4717c7caf278ede6
Geoffrey Cheah <geoffcheah@gmail.com>
2018-03-01 18:07:51 +0000

    created links between user and their reviews and ability to review their coach. added link to new review on booking show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301120124) do
+ActiveRecord::Schema.define(version: 20180301150923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,9 @@ ActiveRecord::Schema.define(version: 20180301120124) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["lesson_id"], name: "index_reviews_on_lesson_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -73,4 +75,5 @@ ActiveRecord::Schema.define(version: 20180301120124) do
   add_foreign_key "bookings", "users"
   add_foreign_key "lessons", "users"
   add_foreign_key "reviews", "lessons"
+  add_foreign_key "reviews", "users"
 end
